de43f52316ac4828e48def8645bdc9eac89b1088
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-04-02 16:00:15 +0200

    Add accessors to Unit

--- java/models/Unit.java
+++ java/models/Unit.java
@@ -4,11 +4,11 @@ package models;
  * Created by Charles on 02/04/2016.
  */
 public class Unit {
-    public int actions;
-    public int health;
-    public UnitType unitType;
-    public int x;
-    public int y;
+    private int actions;
+    private int health;
+    private UnitType unitType;
+    private int x;
+    private int y;
 
     public Unit(UnitType type, int x, int y) {
         this.unitType = type;
@@ -17,4 +17,73 @@ public class Unit {
         this.x = x;
         this.y = y;
     }
+
+    public int getActions(){
+        return this.actions;
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public Unit setActions(int actions){
+        this.actions = actions;
+        return this;
+    }
+
+    public Unit setHealth(int health){
+        this.health = health;
+        return this;
+    }
+
+    public Unit setX(int x){
+        this.x = x;
+        return this;
+    }
+
+    public Unit setY(int y){
+        this.y = y;
+        return this;
+    }
+
+    public UnitType getUnitType(){
+        return this.unitType;
+    }
+
+    public int getMaxActions(){
+        return this.unitType.maxActions;
+    }
+
+    public int getMaxHealth(){
+        return this.unitType.maxHealth;
+    }
+
+    public int getMinRange(){
+        return this.unitType.minRange;
+    }
+
+    public int getMaxRange(){
+        return this.unitType.maxRange;
+    }
+
+    public int getStrength(){
+        return this.unitType.strength;
+    }
+
+    public int getDefense(){
+        return this.unitType.defense;
+    }
+
+    public int getCost(){
+        return this.unitType.cost;
+    }
+
 }
