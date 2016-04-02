2580f9d7ef975da949fdd6bcace0d2dcbcbbe93b
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-04-02 15:55:39 +0200

    Add constructor to Unit

--- java/models/Unit.java
+++ java/models/Unit.java
@@ -4,9 +4,17 @@ package models;
  * Created by Charles on 02/04/2016.
  */
 public class Unit {
-    int actions;
-    int health;
-    UnitType type;
+    public int actions;
+    public int health;
+    public UnitType unitType;
+    public int x;
+    public int y;
 
-
+    public Unit(UnitType type, int x, int y) {
+        this.unitType = type;
+        this.actions = type.maxActions;
+        this.health = type.maxHealth;
+        this.x = x;
+        this.y = y;
+    }
 }
