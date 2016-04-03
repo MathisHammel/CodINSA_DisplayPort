3159391dfea197f67328056b22d2708ff821bc46
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-04-03 02:18:23 +0200

    Check move and destroy

--- java/rules/Rules.java
+++ java/rules/Rules.java
@@ -1,10 +1,7 @@
 package rules;
 
 import algorithms.Utils;
-import models.Cell;
-import models.Game;
-import models.Unit;
-import models.UnitType;
+import models.*;
 
 public class Rules {
 
@@ -34,8 +31,31 @@ public class Rules {
         return true;
     }
 
-    public static boolean checkDestroy(){
-        return false;
+    public static boolean checkDestroy(Game game, int unitId) {
+        Unit unit = game.getCurrentPlayer().getUnit(unitId);
+        if (unit == null) {
+            System.err.println("Cannot destroy: unit " + unitId + " not found");
+            return false;
+        }
+        if(unit.getUnitType() != UnitType.ENGINEER) {
+            System.err.println("Cannot destroy: not an engineer");
+            return false;
+        }
+        Cell target = game.getWorld().getCell(unit.getX(), unit.getY());
+        if (target == null) {
+            System.err.println("Cannot destroy: cell not found");
+            return false;
+        }
+        Building building = target.getBuilding();
+        if (building == Building.NONE) {
+            System.err.println("Cannot destroy: not building");
+            return false;
+        }
+        if(unit.getActions() < 2) {
+            System.err.println("Cannot destroy: not enough actions");
+            return false;
+        }
+        return true;
     }
 
     public static boolean checkMove(Game game, int unitId, int x, int y) {
@@ -44,11 +64,39 @@ public class Rules {
             System.err.println("Cannot move: unit " + unitId + " not found");
             return false;
         }
-        Cell target = game.getWorld().getCell(x, y);
-        if (target == null) {
-            System.err.println("Cannot move: target not found");
+        if (!isCellAccessible(game, unit.getUnitType(), x, y)) {
+            System.err.println("Cannot move: cell is not accessible");
             return false;
         }
-        return Utils.checkMove(target, unit.getX(), unit.getY(), unit.getUnitType(), unit.getActions()) >= 0;
+        int cost = getCellCost(game, game.getWorld().getCell(x, y));
+        if (cost > unit.getActions()) {
+            System.err.println("Cannot move: not enough actions");
+            return false;
+        }
+        return true;
     }
+
+    public static boolean isCellAccessible(Game game, UnitType unitType, int x, int y) {
+        Cell target = game.getWorld().getCell(x, y);
+        if (target == null) {
+            // Cell is out of bounds
+            return false;
+        }
+        if(target.getUnit() != null) {
+            return false; // Cannot move: cell is full
+        }
+        if (target.getLand() == Land.RIVER && target.getBuilding() == Building.BRIDGE && unitType != UnitType.ENGINEER) {
+            return false; // RIVER IS NOT ACCESSIBLE
+        }
+        return false;
+    }
+
+    public static int getCellCost(Game game, Cell target) {
+        int cost = target.getLand() == Land.MONTAIN ? 4 : 2;
+        if(target.getBuilding() == Building.ROAD) {
+            cost /= 2;
+        }
+        return cost;
+    }
+
 }
